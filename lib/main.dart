6ca9a440456d0abf172f4bc24e5305705d5c96aa
Rizky Ramadhan <jmiryas@gmail.com>
2022-08-30 14:44:58 +0700

    Add change theme and info dialog.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
+import '../themes/theme_data_dark.dart';
+// import '../themes/theme_data_light.dart';
+import '../providers/theme_provider.dart';
 import '../screens/main_menu_screen.dart';
 
 void main() {
@@ -11,7 +15,14 @@ void main() {
     DeviceOrientation.portraitDown,
   ]);
 
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,53 +30,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
       title: "Info BMKG",
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: const Color(0xFF488B8F),
-        scaffoldBackgroundColor: const Color(0xFFFAF9F9),
-        appBarTheme: const AppBarTheme(
-          color: Color(0xFF488B8F),
-        ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Color(0xFF488B8F),
-          selectedIconTheme: IconThemeData(
-            color: Color.fromARGB(255, 217, 248, 241),
-          ),
-          selectedItemColor: Color.fromARGB(255, 217, 248, 241),
-          unselectedIconTheme: IconThemeData(
-            color: Color.fromARGB(255, 157, 206, 206),
-          ),
-          unselectedItemColor: Color.fromARGB(255, 157, 206, 206),
-        ),
-        drawerTheme: const DrawerThemeData(
-          backgroundColor: Color(0xFF488B8F),
-        ),
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xFF393E46),
-        appBarTheme: const AppBarTheme(
-          color: Color(0xFF222831),
-        ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Color(0xFF222831),
-          selectedIconTheme: IconThemeData(
-            color: Color(0xFFFFD369),
-          ),
-          selectedItemColor: Color(0xFFFFD369),
-          unselectedIconTheme: IconThemeData(
-            color: Color(0xFFEEEEEE),
-          ),
-          unselectedItemColor: Color(0xFFEEEEEE),
-        ),
-        drawerTheme: const DrawerThemeData(
-          backgroundColor: Color(0xFF222831),
-        ),
-      ),
-      themeMode: ThemeMode.dark,
+      theme: ThemeData.light(),
+      darkTheme: themeDataDark(),
+      themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
       home: const MainMenuScreen(),
     );
   }
--- lib/widgets/app_drawer_widget.dart
+++ lib/widgets/app_drawer_widget.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../providers/theme_provider.dart';
+import '../widgets/custom_alert_dialog_widget.dart';
 
 class AppDrawerWidget extends StatelessWidget {
   const AppDrawerWidget({Key? key}) : super(key: key);
@@ -11,28 +16,81 @@ class AppDrawerWidget extends StatelessWidget {
           ListTile(
             leading: const Icon(
               Icons.info,
-              color: Color(0xFFFAF9F9),
             ),
             title: const Text(
               "Tentang Aplikasi",
-              style: TextStyle(
-                color: Color(0xFFFAF9F9),
-              ),
             ),
-            onTap: () {},
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return CustomAlertDialogWidget(
+                      title: "Tentang Aplikasi",
+                      children: [
+                        const Text(
+                          "Aplikasi ini dibuat oleh Rizky Ramadhan untuk menampilkan informasi cuaca dan gempa dengan menggunakan Flutter. Data yang digunakan bersumber dari website Data Terbuka BMKG.",
+                          textAlign: TextAlign.left,
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        TextButton(
+                          onPressed: () async {
+                            await launchUrl(
+                              Uri.parse("https://data.bmkg.go.id/"),
+                            );
+                          },
+                          child: const Text("Data Terbuka BMKG"),
+                        ),
+                      ],
+                    );
+                  });
+            },
           ),
           ListTile(
             leading: const Icon(
               Icons.palette,
-              color: Color(0xFFFAF9F9),
             ),
             title: const Text(
               "Tema",
-              style: TextStyle(
-                color: Color(0xFFFAF9F9),
-              ),
             ),
-            onTap: () {},
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return CustomAlertDialogWidget(
+                      title: "Tema",
+                      children: [
+                        const Text("Ganti tema yang digunakan?"),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        Consumer<ThemeProvider>(
+                          builder: ((context, value, child) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                value.isDarkMode
+                                    ? const Icon(Icons.dark_mode)
+                                    : const Icon(Icons.sunny),
+                                IconButton(
+                                  onPressed: () async {
+                                    await value
+                                        .toggleIsDarkMode()
+                                        .whenComplete(() {});
+                                  },
+                                  icon: value.isDarkMode
+                                      ? const Icon(Icons.toggle_on)
+                                      : const Icon(Icons.toggle_off),
+                                ),
+                              ],
+                            );
+                          }),
+                        )
+                      ],
+                    );
+                  });
+            },
           ),
         ],
       ),
